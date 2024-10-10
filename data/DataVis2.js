7f9bda7b277ffb713ecc2a76f742ce8f0966fffb
Tay Chean Hao <ctay0026@student.monash.edu>
2024-10-11 02:39:10 +0800

    Completed Data Visualisation.

---
+++ data/DataVis2.js
@@ -0,0 +1,18 @@
+var specVis1 = "./homework_9.json";
+var specVis2 = "./homework_10.json";
+var specVis3 = "./mobile_subscription_bar_chart.json";
+var specVis4 = "./internet_users_pie_chart.json";
+
+vegaEmbed('#vis1', specVis1, { "actions": false });
+
+vegaEmbed('#vis2', specVis2, { "actions": false }).then(function (result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#vis3', specVis3, { "actions": false }).then(function (result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#vis4', specVis4, { "actions": false }).then(function (result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
